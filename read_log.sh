5715869f7eac9a6e2f69f27948de9e6cf5cf898a
root <root@christian-f-brinkmann.de>
2023-07-08 11:17:32 +0200

    small scripts to read the log (when using a screen)

---
+++ read_log.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# screen -L find_duplicates.py ....
+
+grep -B1 -A3 Dry-run screenlog.0
---
+++ read_log_extended.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# screen -L find_duplicates.py ....
+
+grep '^\-\-\->\|Dry-run\|If you are happy\|In total \|^  \-\-' screenlog.0
